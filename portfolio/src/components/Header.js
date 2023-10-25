a0e22875a203d436aa7a3cd90df4e714c8c9aa51
Emma-John <redrose.esj@gmail.com>
2023-10-25 13:12:25 -0600

    Added Files

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+
+function Header() {
+  return (
+    <div>
+    <h3>Header</h3>
+    <Navbar />
+    </div>
+  )
+}
+
+export default Header
---
+++ portfolio/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+    <>
+    <div>Home</div>
+    <Footer />
+    </>
+  )
+}
+
+export default Home
